056cb604a979464b8e2b8cca2b5ba5a0b747a97b
MehSwitch <aymehaiDev@protonmail.com>
2018-06-20 14:08:19 -0400

    Optimized code by removing a for loop

--- RollDice/ViewController.swift
+++ RollDice/ViewController.swift
@@ -10,6 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnRoll: UIButton!
+    @IBOutlet weak var imgDice: UIImageView!
+    @IBOutlet weak var imgDice2: UIImageView!
+    
+    
+    @IBAction func btnRollAction(_ sender: Any) {
+        let randomNum1:Int = Int(arc4random_uniform(5)) + 1
+        let randomNum2:Int = Int(arc4random_uniform(5)) + 1
+        var hopAnime: [UIImage] = []
+        var hopAnime2: [UIImage] = []
+        
+        for _ in 0...6 {
+            let randomNum:Int = Int(arc4random_uniform(5)) + 1
+            let randomNum2:Int = Int(arc4random_uniform(5)) + 1
+            hopAnime.append(UIImage(named: "dice\(randomNum).jpg")!)
+            hopAnime2.append(UIImage(named: "dice\(randomNum2).jpg")!)
+        }
+
+        
+        imgDice.animationImages = hopAnime
+        imgDice2.animationImages = hopAnime2
+        
+        imgDice.animationDuration = 1.0
+        imgDice2.animationDuration = 1.0
+        
+        imgDice.animationRepeatCount = 2
+        imgDice2.animationRepeatCount = 2
+        
+        imgDice.startAnimating()
+        imgDice2.startAnimating()
+        
+        imgDice.image = UIImage(named: "dice\(randomNum1).jpg")
+        imgDice2.image = UIImage(named: "dice\(randomNum2).jpg")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
